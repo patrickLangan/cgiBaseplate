54666ba4a1c916d1a5a3300027deb67cc81ea56a
patrickLangan <patrick.g.langan@gmail.com>
2014-08-07 18:22:01 -0400

    Removed file

--- spy.c
+++
@@ -1,9 +0,0 @@
-#include "fcgi_stdio.h"
-#include <stdlib.h>
-
-int main (int argc, char **argv)
-{
-	while (FCGI_Accept() >= 0)
-		printf("Content-type: text/html\r\n\r\n"
-		       "%s", getenv("REMOTE_ADDR"));
-}
