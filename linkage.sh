217daef227066177d80b5303b48a8e61519426f4
root <root@ubuntu.(none)>
2014-08-09 21:02:30 -0400

    New path to nginx conf

--- linkage.sh
+++ linkage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-rm /etc/nginx/nginx.conf
-cp nginx.conf /etc/nginx
+rm /usr/local/nginx/conf/nginx.conf
+cp nginx.conf /usr/local/nginx/conf
 
-rm /etc/nginx/fastcgi_params
-cp fastcgi_params /etc/nginx
+rm /usr/local/nginx/conf/fastcgi_params
+cp fastcgi_params /usr/local/nginx/conf
 
 rm /www/index.html
 cp index.html /www
+
+/usr/local/nginx/sbin/nginx -s reload
 
