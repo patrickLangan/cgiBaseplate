0fa144ae5304487313becbdf7e9a579ecd3e4bab
patrickLangan <patrick.g.langan@gmail.com>
2014-08-10 22:11:40 -0400

    Reads post values, moves upload file

--- upload.c
+++ upload.c
@@ -1,31 +1,79 @@
 #include "fcgi_stdio.h"
 #include <stdlib.h>
+#include <string.h>
+
+/* todo
+ * Error checking, handling, and prevention
+ */
+
+int readPostMultipart (char *postVal)
+{
+	char line[255];
+	size_t length;
+
+	//Read and ignore the first line
+	FCGI_fgets (line, 255, FCGI_stdin);
+
+	//Check the header type
+	FCGI_fread (line, 38, 1, FCGI_stdin);
+	line[38] = '\0';
+	if ((strcmp (line, "Content-Disposition: form-data; name=\"")))
+		return 0;
+
+	//Read and ignore up until the last line
+	FCGI_fgets (line, 255, FCGI_stdin);
+	FCGI_fgetc (FCGI_stdin);
+	FCGI_fgetc (FCGI_stdin);
+
+	//Read the value
+	FCGI_fgets (postVal, 255, FCGI_stdin);
+
+	//Remove the newline
+	length = strlen (postVal) - 2;
+	postVal[length] = '\0';
+
+	return 1;
+}
 
 int main (int argc, char **argv)
 {
 	unsigned int length;
-	char *buffer;
 	FCGI_FILE *file;
+
+	char *fileName = malloc (255);
+	char *fileType = malloc (255);
+	char *filePath = malloc (255);
+	char *fileHash = malloc (255);
+	char *fileSize = malloc (255);
+
+	char *newFilePath = malloc (255);
 
 	while (1) {
 		//The program will wait inside of FCGI_Accept untill it gets a new request
 		FCGI_Accept ();
 
-		//Writing the post request to a file
-		length = atoi (getenv ("CONTENT_LENGTH"));
-		buffer = malloc (length);
-		FCGI_fread (buffer, length, 1, FCGI_stdin);
-		file = FCGI_fopen ("uploadRequest.txt", "w");
-		FCGI_fwrite (buffer, length, 1, file);
-		FCGI_fclose (file);
-
 		//Response
 		FCGI_printf ("Content-type: text/html\r\n\r\n");
-		FCGI_printf ("<p>Files have been uploaded.<p>\n");
+
+		//Read file info and move file to permanent location
+		while  (readPostMultipart (fileName)) {
+			readPostMultipart (fileType);
+			readPostMultipart (filePath);
+			readPostMultipart (fileHash);
+			readPostMultipart (fileSize);
+
+			sprintf (newFilePath, "/www/upload/%s", fileName);
+			rename (filePath, newFilePath);
+		}
 
 		//Flush and free
 		FCGI_Finish ();
-		free (buffer);
+		free (fileName);
+		free (fileType);
+		free (filePath);
+		free (fileHash);
+		free (fileSize);
+		free (newFilePath);
 	}
 }
 
